d226b094607fc2715c33df200c4300f32b9985c0
krishna awasthi <141904373+Krishnawasthi@users.noreply.github.com>
2026-07-18 17:28:16 +0530

    Update Payment.java

--- Inheritance/src/com/updowncasting/payment/Payment.java
+++ Inheritance/src/com/updowncasting/payment/Payment.java
@@ -1,3 +1,26 @@
+/*📄 1. Payment.java
+Purpose
+
+The Payment class acts as the parent (super) class for all payment methods.
+
+What are we achieving?
+1. Creating a common blueprint for all payment types.
+2. Providing a common method (pay()) that every payment method must have.
+3. Achieving Inheritance.
+4. Achieving Method Overriding by allowing child classes to provide their own implementation. 
+5. Each child class overrides the pay() method and also contains its own unique functionality.
+
+Example:
+
+CreditCard → generatePin()
+UPI → scanQRCode()
+NetBanking → loginToBank()
+
+This demonstrates Runtime Polymorphism, because calling pay() executes the version belonging to the actual object.*/
+
+
+
+
 package com.updowncasting.payment;
 
 
